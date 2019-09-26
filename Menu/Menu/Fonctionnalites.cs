87c2957f28b646c6fe648af9feb50832c2fe4e2e
Samir Kamar <skamar@tu.unistra.fr>
2019-09-26 17:04:56 +0200

    Mise en place MCD

---
+++ Menu/Menu/Fonctionnalites.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Menu
+{
+    public class Fonctionnalites
+    {
+        private string nom { get; set; }
+        private int paNecces { get; set; }
+        private int pourcentNote { get; set; }
+        private int nvConnaissNecces { get; set; }
+    }
+}
---
+++ Menu/Menu/Projet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Menu
+{
+    public class Projet
+    {
+        private int tempsLimite { get; set; }
+
+        private int nom { get; set; }
+    }
+}
