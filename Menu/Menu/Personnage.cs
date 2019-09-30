b53464daac55515b67b3da0d8fe4d60da78257d4
Mathieu Cisse <mathieu.cisse@etu.unistra.fr>
2019-09-30 16:45:44 +0200

    Ajout des classes : Fonctionnalites, Projet, Personnage

--- Menu/Menu/Personnage.cs
+++ Menu/Menu/Personnage.cs
@@ -2,48 +2,138 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace Menu
+namespace Personnage
 {
     public class Personnage
     {
-        private int id { get; set; }
-        private int fatigue { get; set; }
-        private int nvSavoir { get; set; }
-        private int pA { get; set; }
-        private int prenom { get; set; }
-        private int sociabilite { get; set; }
-        private int stress { get; set; }
-        private int tempsConsacre { get; set; }
+        //Attributs d'un personnage 
+        private int id;
+        private string prenom;
+        private int fatigue = 0;
 
-        public void calculFatigue()
+        private int productivite;
+        private int stress;
+        private int connaissances = 0;
+        private int sociabilite;
+        private int pA;
+               
+
+        //Constructeur de la classe personnage
+        public Personnage(string prenom, int productivite, int stress, int sociabilite)
         {
-            throw new System.NotImplementedException();
+            this.prenom = prenom;
+            this.productivite = productivite;
+            this.stress = stress;
+            this.sociabilite = sociabilite;
         }
 
-        public void calculNvSavoir()
+        //Accesseurs (pour les set, les valeurs acceptées sont comprises entre 0 et 100)
+        public string getPrenom()
         {
-            throw new System.NotImplementedException();
+            return this.prenom;
+        }
+        public void setPrenom(string prenom)
+        {
+            this.prenom = prenom;
         }
 
-        public void calculPA()
+        public int getId()
         {
-            throw new System.NotImplementedException();
+            return this.id;
+        }
+       
+
+        public int getPA()
+        {
+            return this.pA;
+        }
+        public void setPA(int pA)
+        {
+            this.pA = pA;
         }
 
-        public void calculSociabilite()
+
+        public int getFatigue()
         {
-            throw new System.NotImplementedException();
+            return this.fatigue;
+        }
+        public void setFatigue(int fatigue)
+        {
+            if (fatigue >= 0 && fatigue <= 100)
+            {
+                this.fatigue = fatigue;
+            }
+            else if (fatigue < 0)
+            {
+                this.fatigue = 0;
+            }
+            else
+            {
+                this.fatigue = 100;
+            }
+
+        }
+        public int getStress()
+        {
+            return this.stress;
+        }
+        public void setStress(int stress)
+        {
+            if (stress >= 0 && stress <= 100)
+            {
+                this.stress = stress;
+            }
+            else if (stress < 0)
+            {
+                this.stress = 0;
+            }
+            else
+            {
+                this.stress = 100;
+            }
         }
 
-        public void calculStress()
+        public int getConnaissances()
         {
-            throw new System.NotImplementedException();
+            return this.connaissances;
+        }
+        public void setConnaissances(int connaissances)
+        {
+            if (connaissances >= 0 && connaissances <= 100)
+            {
+                this.connaissances = connaissances;
+            }
+            else if (connaissances < 0)
+            {
+                this.connaissances = 0;
+            }
+            else
+            {
+                this.connaissances = 100;
+            }
         }
 
-        public void faireAction(Action action)
+        public int getSociabilite()
         {
-            throw new System.NotImplementedException();
+            return this.sociabilite;
+        }
+        public void setSociabilite(int sociabilite)
+        {
+            if (sociabilite >= 0 && sociabilite <= 100)
+            {
+                this.sociabilite = sociabilite;
+            }
+            else if (sociabilite < 0)
+            {
+                this.sociabilite = 0;
+            }
+            else
+            {
+                this.sociabilite = 100;
+            }
         }
     }
+
 }
