18ea44f1b76e5605d2e8274bfa6de6d395ac7921
Samir Kamar <skamar@tu.unistra.fr>
2019-11-17 12:49:12 +0100

    Gestion bouton reunion et maladie

---
+++ Menu/Menu/Forms/frmButReunion.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Menu.Forms
+{
+    public partial class frmButReunion : Form
+    {
+        public frmButReunion()
+        {
+            InitializeComponent();
+        }
+
+        public void themeReunion(String theme)
+        {
+            int posGauche = 66;
+
+            if (theme == "Découvir l'équipe")
+            {
+                Button button1 = new Button();
+                button1.Size = new System.Drawing.Size(494, 78);
+                button1.Location = new System.Drawing.Point(posGauche, 38);
+                button1.Text = "Faire connaissance";
+                button1.Cursor = System.Windows.Forms.Cursors.Hand;
+                button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(29)))), ((int)(((byte)(50)))));
+                button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                button1.ForeColor = System.Drawing.Color.Orange;
+                button1.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                button1.Click += new System.EventHandler(this.lancerNotification);
+
+                Button button2 = new Button();
+                button2.Size = new System.Drawing.Size(494, 78);
+                button2.Location = new System.Drawing.Point(posGauche, 138);
+                button2.Text = "Évaluer le potentiel du groupe";
+                button2.Cursor = System.Windows.Forms.Cursors.Hand;
+                button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(29)))), ((int)(((byte)(50)))));
+                button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                button2.ForeColor = System.Drawing.Color.Orange;
+                button2.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                button2.Click += new System.EventHandler(this.lancerNotification);
+                
+                Button button3 = new Button();
+                button3.Size = new System.Drawing.Size(494, 78);
+                button3.Location = new System.Drawing.Point(posGauche, 238);
+                button3.Text = "Révèle les qualités et les défauts";
+                button3.Cursor = System.Windows.Forms.Cursors.Hand;
+                button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(29)))), ((int)(((byte)(50)))));
+                button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                button3.ForeColor = System.Drawing.Color.Orange;
+                button3.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                button3.Click += new System.EventHandler(this.lancerNotification);
+
+                Button button4 = new Button();
+                button4.Size = new System.Drawing.Size(494, 78);
+                button4.Location = new System.Drawing.Point(posGauche, 338);
+                button4.Text = "Analyse des affinités de chacun";
+                button4.Cursor = System.Windows.Forms.Cursors.Hand;
+                button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(29)))), ((int)(((byte)(50)))));
+                button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                button4.ForeColor = System.Drawing.Color.Orange;
+                button4.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                button4.Click += new System.EventHandler(this.lancerNotification);
+
+                this.Controls.Add(button1);
+                this.Controls.Add(button2);
+                this.Controls.Add(button3);
+                this.Controls.Add(button4);
+            }
+
+            if(theme == "Examiner le projet") {
+                Button button1 = new Button();
+                button1.Size = new System.Drawing.Size(494, 78);
+                button1.Location = new System.Drawing.Point(posGauche, 38);
+                button1.Text = "Découvrir le projet";
+                button1.Cursor = System.Windows.Forms.Cursors.Hand;
+                button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(29)))), ((int)(((byte)(50)))));
+                button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                button1.ForeColor = System.Drawing.Color.Orange;
+                button1.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                button1.Click += new System.EventHandler(this.lancerNotification);
+
+                Button button2 = new Button();
+                button2.Size = new System.Drawing.Size(494, 78);
+                button2.Location = new System.Drawing.Point(posGauche, 138);
+                button2.Text = "Analyse de la demande du client et de ses besoins";
+                button2.Cursor = System.Windows.Forms.Cursors.Hand;
+                button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(29)))), ((int)(((byte)(50)))));
+                button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                button2.ForeColor = System.Drawing.Color.Orange;
+                button2.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                button2.Click += new System.EventHandler(this.lancerNotification);
+
+                Button button3 = new Button();
+                button3.Size = new System.Drawing.Size(494, 78);
+                button3.Location = new System.Drawing.Point(posGauche, 238);
+                button3.Text = "Définir un cahier des charges";
+                button3.Cursor = System.Windows.Forms.Cursors.Hand;
+                button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(29)))), ((int)(((byte)(50)))));
+                button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                button3.ForeColor = System.Drawing.Color.Orange;
+                button3.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                button3.Click += new System.EventHandler(this.lancerNotification);
+
+                Button button4 = new Button();
+                button4.Size = new System.Drawing.Size(494, 78);
+                button4.Location = new System.Drawing.Point(posGauche, 338);
+                button4.Text = "Mettre en place un système d'organisation";
+                button4.Cursor = System.Windows.Forms.Cursors.Hand;
+                button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(29)))), ((int)(((byte)(50)))));
+                button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                button4.ForeColor = System.Drawing.Color.Orange;
+                button4.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                button4.Click += new System.EventHandler(this.lancerNotification);
+
+                Button button5 = new Button();
+                button5.Size = new System.Drawing.Size(494, 78);
+                button5.Location = new System.Drawing.Point(posGauche, 338);
+                button5.Text = "Mettre en place un calendrier";
+                button5.Cursor = System.Windows.Forms.Cursors.Hand;
+                button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(29)))), ((int)(((byte)(50)))));
+                button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                button5.ForeColor = System.Drawing.Color.Orange;
+                button5.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                button5.Click += new System.EventHandler(this.lancerNotification);
+
+                this.Controls.Add(button1);
+                this.Controls.Add(button2);
+                this.Controls.Add(button3);
+                this.Controls.Add(button4);
+                this.Controls.Add(button5);
+            }
+
+            if(theme == "Faire le point")
+            {
+                Button button1 = new Button();
+                button1.Size = new System.Drawing.Size(494, 78);
+                button1.Location = new System.Drawing.Point(posGauche, 38);
+                button1.Text = "Mettre en commun le travail et l'avancement de chacun";
+                button1.Cursor = System.Windows.Forms.Cursors.Hand;
+                button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(29)))), ((int)(((byte)(50)))));
+                button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                button1.ForeColor = System.Drawing.Color.Orange;
+                button1.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                button1.Click += new System.EventHandler(this.lancerNotification);
+
+                this.Controls.Add(button1);
+            }
+
+        }
+
+
+        public void lancerNotification(object sender, EventArgs e)
+        {
+            this.Opacity = 0.3;     //effet flouté pour le popup
+            frmReunionPopUp popup = new frmReunionPopUp();
+            DialogResult dr = new DialogResult();
+            dr=popup.ShowDialog();
+            if(popup.DialogResult == DialogResult.Cancel)
+            {
+                //enlever l'effet flouté
+                this.Opacity = 1;
+            }
+
+            //une fois l'achievement activé on bloque le boutton avec un enable pour dire à l'utilisateur 
+            //qu'il n'a plus besoin de s'occuper de cet achievement et que cette derniere a été validé
+            Button b = (Button)sender;
+            b.Enabled = false;  
+        }
+
+        private void btnRevenir_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Menu/Menu/Forms/frmReunionPopUp.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Menu.Forms
+{
+    partial class frmReunionPopUp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNotification = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNotification
+            // 
+            this.lblNotification.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblNotification.AutoSize = true;
+            this.lblNotification.Font = new System.Drawing.Font("Cooper Black", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNotification.ForeColor = System.Drawing.Color.Black;
+            this.lblNotification.Location = new System.Drawing.Point(226, 9);
+            this.lblNotification.Name = "lblNotification";
+            this.lblNotification.Size = new System.Drawing.Size(330, 55);
+            this.lblNotification.TabIndex = 1;
+            this.lblNotification.Text = "Félicitations";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblInfo.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.ForeColor = System.Drawing.Color.Black;
+            this.lblInfo.Location = new System.Drawing.Point(0, 76);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(800, 165);
+            this.lblInfo.TabIndex = 2;
+            this.lblInfo.Text = "notif dans lequel on annonce à l\'utilisateur qu\'il a débloqué qqch genre par exem" +
+    "ple \"Félécitation mtn vous connaissez le prénom de vos personnages ainsi que leu" +
+    "r statut\"";
+            this.lblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnOk
+            // 
+            this.btnOk.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnOk.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOk.FlatAppearance.BorderSize = 0;
+            this.btnOk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOk.Font = new System.Drawing.Font("Cooper Black", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOk.ForeColor = System.Drawing.Color.White;
+            this.btnOk.Location = new System.Drawing.Point(739, 194);
+            this.btnOk.Margin = new System.Windows.Forms.Padding(0);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(61, 47);
+            this.btnOk.TabIndex = 4;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = false;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // frmReunionPopUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(800, 241);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.lblNotification);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmReunionPopUp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmReunionDecouverteEquipe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNotification;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Button btnOk;
+    }
+}
